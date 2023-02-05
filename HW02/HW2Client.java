eb562ab3ebad0047df4c8161919ae5d7b5ac0238
Ling Thang <Lingthang03@gmail.com>
2023-02-04 21:19:47 -0700

    updated readme

--- HW02/HW2Client.java
+++ HW02/HW2Client.java
@@ -6,7 +6,6 @@
 
 import java.io.*;
 import java.net.*;
-import java.util.*;
 
 public class HW2Client {
     public static void main(String[] args) throws IOException {
@@ -20,11 +19,31 @@ public class HW2Client {
         DatagramSocket udpSocket = new DatagramSocket();
 
         BufferedReader sysIn = new BufferedReader(new InputStreamReader(System.in));
+        String line;
         String fromServer;
         String fromUser;
 
+        // send request
+        InetAddress address = InetAddress.getByName(args[0]);
         System.out.println("");
-        System.out.println("	             Welcome to the UDP Client App!");
+        System.out.println("	             DNS/IP Address Check!");
+        System.out.println("=========================================================");
+        System.out.println("    Before we begin, please make sure that your Address is correct.");
+        System.out.println("    this is your DNS/IP Address: " + address);
+        System.out.println("    If you do not recognize this address, Please enter your DNS/IP Address: ");
+        System.out.println("    Other wise press enter to continue:");
+        while ((line = sysIn.readLine()) != null) {
+            if (line.isEmpty()) {
+                break;
+            } else {
+                address = InetAddress.getByName(line);
+                System.out.println("Your DNS/IP Address is: " + address);
+                break;
+            }
+        }
+
+        System.out.println("");
+        System.out.println("	             Welcome to the UDP Client Server!");
         System.out.println("	========================================================");
         System.out.println("	Here is a List of items you can request from the server:");
         System.out.println("	Item ID                                  Item Description");
@@ -39,17 +58,15 @@ public class HW2Client {
 
             if (fromUser.equals("00001") || fromUser.equals("00002") || fromUser.equals("00003")
                     || fromUser.equals("00004") || fromUser.equals("00005") || fromUser.equals("00006")) {
-                System.out.println("You have requested for item: " + fromUser);
+                System.out.println("\n You have requested for item: " + fromUser);
             } else {
-                System.out.println("Invalid Item ID. Please try again.");
+                System.out.println("\n Invalid Item ID. Please try again.");
                 continue;
             }
 
             // display user input
-            System.out.println("From Client: " + fromUser);
+            System.out.println("Fetching Information for: " + fromUser);
 
-            // send request
-            InetAddress address = InetAddress.getByName(args[0]);
             byte[] buf = fromUser.getBytes();
             DatagramPacket udpPacket = new DatagramPacket(buf, buf.length, address, 5270); // my port number
             udpSocket.send(udpPacket);
